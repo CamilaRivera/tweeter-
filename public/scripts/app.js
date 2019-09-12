d379c517aae3b72c212dec332ad567fe7c0e2d7d
CamilaRivera <camila.riverart@gmail.com>
2019-09-12 00:16:32 +0000

    Implement escape function for prevent Cross-Site Scripting

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -11,6 +11,12 @@ jQuery(document).ready(function ($) {
     return `${Math.round((Date.now() - new Date(unixTimestamp)) / (1000 * 60 * 60 * 24))} days ago`;
   };
 
+  const escape =  function(str) {
+    let div = document.createElement('div');
+    div.appendChild(document.createTextNode(str));
+    return div.innerHTML;
+  }
+
   /** */
   // loops through tweets
   // calls createTweetElement for each tweet
@@ -31,11 +37,11 @@ jQuery(document).ready(function ($) {
       `<article class='tweet'>
         <header>
           <p class='nickname'>${tweet.user.handle}</p>
-          <p><img src=${tweet.user.avatars} width='42' height='42'>${tweet.user.name}</p>
+          <p><img src=${escape(tweet.user.avatars)} width='42' height='42'>${escape(tweet.user.name)}</p>
         </header>
-        <p>${tweet.content.text}</p>
+        <p>${escape(tweet.content.text)}</p>
         <footer>
-          <span>${getDaysAgo(tweet.created_at)}</span>
+          <span>${getDaysAgo(escape(tweet.created_at))}</span>
           <p class='icons'><i class='icon-flag'></i><i class='icon-retweet'></i><i class='icon-heart'></i></p>
         </footer>
     </article>`
@@ -52,7 +58,7 @@ jQuery(document).ready(function ($) {
       alert('content must be shorter than 140 characters');
     }
     if ($('.tweet-text').val().length === 0) {
-      alert('you must fill these form');
+      alert('you must fill this form');
     } else {
       $.ajax({ url: '/tweets', method: 'POST', data: $(this).serialize() })
         .then(
